c667500b97c36b8cc12898bc86fb99b82c646280
Daniel <Dannxzone@gmail.com>
2018-06-15 00:23:58 -0400

    Rogue Demo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,112 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public float turnDelay = .1f;
+    public float levelStartDelay = 2f;
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    public int playerfoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+
+    private GameObject levelImage;
+    private Text levelText;
+    private int level = 1;
+    private List<Enemy> enemies;
+    private bool doingSetup;
+    private bool enemiesMoving;
+
+
+	void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+    
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        level++;
+        InitGame();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void InitGame()
+    {
+        doingSetup = true;
+
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+	
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    public void GameOver()
+    {
+        levelText.text = "After " + level + " days, you died";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+    
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies(){
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+
+    void Update()
+    {
+        if (playersTurn || enemiesMoving || doingSetup)
+            return;
+        
+
+        StartCoroutine (MoveEnemies());
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Player : MovingObject {
+
+    public Text foodText;
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartlevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        food = GameManager.instance.playerfoodPoints;
+
+        foodText.text = "Food " + food;
+
+        base.Start();
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.playerfoodPoints = food;
+    }
+
+    void Update () {
+        if (!GameManager.instance.playersTurn) return;
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0)
+            vertical = 0;
+
+        if (horizontal != 0 || vertical != 0)
+            AttemptMove<Wall>(horizontal, vertical);
+	}
+
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+        foodText.text = "Food " + food;
+
+        base.AttemptMove<T>(xDir, yDir);
+        RaycastHit2D hit;
+
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    // Normal Trigger !!!!!!!!!
+    private void OnTriggerEnter2D(Collider2D o)
+    {
+        if (o.tag == "Exit")
+        {
+            Invoke("Restart", restartlevelDelay);
+            enabled = false;
+        } else if (o.tag == "Food") {
+            food += pointsPerFood;
+            foodText.text = "+" + pointsPerFood + " Food:" + food;
+            o.gameObject.SetActive(false);
+        } else if (o.tag == "Soda") {
+            food += pointsPerSoda;
+            foodText.text = "+" + pointsPerSoda + " Food:" + food;
+            o.gameObject.SetActive(false);
+        }
+    }
+
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitwall = component as Wall;
+        hitwall.DamageWall(wallDamage);
+        animator.SetTrigger("playerChop");
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoseFood (int loss)
+    {
+        animator.SetTrigger("playerHit");
+        food -= loss;
+        foodText.text = "-" + loss + " Food:" + food;
+        CheckIfGameOver();
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (food <= 0) {
+            GameManager.instance.GameOver();
+        }
+    }
+}
